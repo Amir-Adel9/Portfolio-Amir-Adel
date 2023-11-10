a2edb06a34eec57356de3d3e2c7768d6fe2541cf
Amir-Adel9 <amiradel7152@gmail.com>
2023-11-10 03:15:03 +0200

    Update Astro config and package versions, and drive links

--- src/components/react/Hero.tsx
+++ src/components/react/Hero.tsx
@@ -129,7 +129,7 @@ const Hero: React.FC<{ children: ReactNode }> = (props) => {
                 >
                   <div>
                     <a
-                      href='https://drive.google.com/file/d/1QwfFaBky8AvjiEMeIvp4_08RlKn5aUL6/view?usp=sharing'
+                      href='https://drive.google.com/file/d/1FB-bBMzj7aZlNyuFmM-ih1zwx2rPhtzb/view?usp=sharing'
                       className='flex items-center'
                       target={'_blank'}
                     >
